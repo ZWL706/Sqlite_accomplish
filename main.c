39c2a5537e7abd5aea53b51520a189f48c2c58f4
ZWL <1007269773@qq.com>
2022-06-29 22:50:05 +0800

    第二次任务

--- main.c
+++ main.c
@@ -5,13 +5,37 @@
 #include <stdbool.h>
 // getline中使用的库
 #include <errno.h>
+//用户输入
 typedef struct
 {
     char *buffer;
     size_t buffer_length;
     size_t input_length;
 } InputBuffer;
-
+//虚拟机返回结果
+//原命令结果
+typedef enum
+{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+//事先对语句的判别结果：可以识别的语句，不可以识别的语句
+typedef enum
+{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+// sql语句
+// sql语句类型：增删改查
+typedef enum
+{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+typedef struct
+{
+    StatementType type;
+} Statement;
 //初始化
 InputBuffer *new_input_buffer()
 {
@@ -90,6 +114,48 @@ void close_input_buffer(InputBuffer *input_buffer)
     free(input_buffer);
 }
 
+//识别命令
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+// 对用户输入的命令识别属于哪种sql（增删改查）
+PrepareResult prepare_statement(InputBuffer *input_buffer,
+                                Statement *statement)
+{
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+// sql语句执行
+void execute_statement(Statement *statement)
+{
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do a select.\n");
+        break;
+    }
+}
 int main(int argc, char *argv[])
 {
     InputBuffer *inputBuffer = new_input_buffer();
@@ -97,14 +163,37 @@ int main(int argc, char *argv[])
     {
         print_promt();
         read_input(inputBuffer);
-        if (strcmp(inputBuffer->buffer, ".exit") == 0)
+        //非sql语句都为.xxx形式
+        if (inputBuffer->buffer[0] == '.')
         {
-            close_input_buffer(inputBuffer);
-            exit(EXIT_SUCCESS);
+            switch (do_meta_command(inputBuffer))
+            {
+                //当switch外面有循环语句时可以用循环语句的特性来结束该case
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command '%s'\n", inputBuffer->buffer);
+                continue;
+
+            default:
+                break;
+            }
         }
-        else
+        Statement statement;
+        //判断语句类型
+        switch (prepare_statement(inputBuffer, &statement))
         {
-            printf("Unrecognized command  '%s'. \n ", inputBuffer->buffer);
+        case PREPARE_SUCCESS:
+            continue;
+        case PREPARE_UNRECOGNIZED_STATEMENT:
+            //无法识别的sql关键字
+            printf("Unrecognized keyword at start of '%s'. \n", inputBuffer->buffer);
+            continue;
+        default:
+            break;
         }
+        //执行语句
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
 }
